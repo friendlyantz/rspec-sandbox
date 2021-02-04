5ea67b237f01a3a5ee367c6e76c017c60b2148de
Anton <panteleev23@gmail.com>
2021-02-04 13:17:00 +1100

    refactoring

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -8,13 +8,15 @@ class Card
 end
 
 RSpec.describe Card do
+  before do
+    @card = Card.new('Ace', 'Spades')
+  end
+
   it 'has a rank' do
-    card = Card.new('Ace', 'Spades')
-    expect(card.rank).to eq('Ace')  
+    expect(@card.rank).to eq('Ace')  
   end
 
   it 'has a suit' do
-    card = Card.new('Ace', 'Spades')
-    expect(card.suit).to eq('Spades')  
+    expect(@card.suit).to eq('Spades')  
   end
 end
