28706bf4445b14b0c03045cd6570269f06a12b19
Anton <panteleev23@gmail.com>
2021-02-05 18:28:02 +1100

    custom error msg

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,5 +1,5 @@
 class Card
-  attr_reader :rank, :suit
+  attr_accessor :rank, :suit
 
   def initialize(rank, suit)
     @rank = rank
@@ -8,15 +8,20 @@ class Card
 end
 
 RSpec.describe Card do
-  before do
-    @card = Card.new('Ace', 'Spades')
-  end
+  let(:card) { Card.new('Ace', 'Spades') }
 
   it 'has a rank' do
-    expect(@card.rank).to eq('Ace')  
+    expect(card.rank).to eq('Ace')
+    card.rank = 'Queen'
+    expect(card.rank).to eq('Queen')
   end
 
   it 'has a suit' do
-    expect(@card.suit).to eq('Spades')  
+    expect(card.suit).to eq('Spades')  
+  end
+
+  it 'has custom error msg' do
+    comparison ='Spade'
+    expect(card.suit).to eq(comparison), "I expect #{comparison}, not the #{card.suit}"  
   end
 end
