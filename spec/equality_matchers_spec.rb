9dda3829e8ee06bc6c11cbde3f95d3d619ff32cc
friendlyantz <70934030+friendlyantz@users.noreply.github.com>
2023-10-13 19:19:23 +1100

    Add equal matcher for object identity check

--- spec/equality_matchers_spec.rb
+++ spec/equality_matchers_spec.rb
@@ -1,11 +1,15 @@
 # frozen_string_literal: true
 
 describe 'equality' do
-  let(:a) { 3  }
-    it "eq ignores type, eql doesnt" do
-      expect(a).to eq(3)
-      expect(a).to eq(3.0)
-      expect(a).not_to eql(3.0)
-      expect(a).not_to be(3.0) # but be seems a better, less confusing mathcer?
-    end
+  let(:a) { 3 }
+  it 'eq ignores type, eql doesnt' do
+    expect(a).to eq(3)
+    expect(a).to eq(3.0)
+    expect(a).not_to eql(3.0)
+
+    expect([1, 2, 3]).to eq([1, 2, 3])
+    expect([1, 2, 3]).not_to equal([1, 2, 3])
+
+    expect(a).not_to be(3.0) # but be seems a better, less confusing mathcer?
+  end
 end
